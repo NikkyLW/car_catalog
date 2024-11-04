4ab9f5d705cf5cdf028d1e22f3726501284c82da
NikkyLW <korobeynikov_sergey@vk.com>
2024-11-05 04:33:35 +1000

    fix

--- api/urls.py
+++ api/urls.py
@@ -6,7 +6,7 @@ from api import views
 app_name = 'api'
 
 urlpatterns = [
-    path('cars/', CarCreateAPI.as_view(), name='cars-create'),
+    path('car/create/', CarCreateAPI.as_view(), name='cars-create'),
     path('cars/', CarListAPI.as_view(), name='cars-list'),
     path('cars/<int:pk>/', CarDetailAPI.as_view(), name='car-detail'),
     path('cars/<int:pk>/comments/', CommentAPI.as_view(), name='comment-detail'),
